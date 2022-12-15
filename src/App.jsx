4e348945910810c2fe418c5c6140b77a335590ef
Isac Vidal <isac.a.vidal.leite@avanade.com>
2022-12-15 20:27:26 -0300

    Commit Projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post"
+
+export function App() {
+
+  return (
+    <div>
+      <Post author="Isac Vidal" content ="Esposo"/>
+      <Post author="Mariana Terrenas" content ="Esposa"/>
+    </div>
+  )
+}
+
+
